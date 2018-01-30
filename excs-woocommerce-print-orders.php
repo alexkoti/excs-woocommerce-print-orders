a099c7bcb3ed48c5c8a931a173a8706dcb83c956
Alex Koti <alexkoti@gmail.com>
2018-01-30 01:11:44 -0200

    Arquivo inicial

---
+++ excs-woocommerce-print-orders.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: Excs WooCommerce Print Orders
+ * Plugin URI:  http://github.com/alexkoti/excs-woocommerce-print-orders
+ * Description: Allow multiple pickup locations shipping method.
+ * Author:      Alex Koti
+ * Author       URI: http://alexkoti.com/
+ * Version:     1.0.0
+ * License:     GPLv2 or later
+ * Text Domain: excs-woocommerce-print-orders
+ * Domain Path: languages/
+ *
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+
+
